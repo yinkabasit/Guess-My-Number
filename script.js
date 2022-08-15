e48ef0f982d471ff724668093dd5136c37fa6e60
Abdulbasit Olayinka <66054214+yinkabasit@users.noreply.github.com>
2022-08-15 21:42:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 'user script';
 
-/*console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🏆Corrrect Number';
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = '13';
-document.querySelector('.score').textContent = '10';
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);*/
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
@@ -44,29 +35,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.highscore').textContent = highscore;
   }
 
-  /*  //When guess is too much
-  else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high';
-      score = score - 1;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = ' 💥You lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-  }
-
-  //When guess is Low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too Low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥You Lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-  }*/
+ 
 
   //When player is wrong
   else if (guess !== secretNumber) {
